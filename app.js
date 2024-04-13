c0b11221e56d0211bd44d7e18d31edbf54fca014
Guillermofr29 <guillermofr2912@gmail.com>
2024-04-13 16:33:48 -0500

    up: agregado de la ruta de employees

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import morgan from 'morgan';
 // Rutas
 import catRoutes from './src/routes/catalogos.busquedas.routes.js';
 import adminRoutes from './src/routes/admin.routes.js';
-// import employeesRoutes from './src/routes/employees.routes.js';
+import employeesRoutes from './src/routes/employees.routes.js';
 
 // Base de datos
 import { Connection } from './src/database/mysql.database.js';
@@ -37,7 +37,7 @@ const App = {
 
 		app.use('/api/v1/catalogo', catRoutes);
 		app.use('/api/v1/admins', adminRoutes);
-		// app.use('/api/v1/employees', employeesRoutes);
+		app.use('/api/v1/employees', employeesRoutes);
 		app.use('/api/v1/test', (req, res) => {
 			res.status(200).json({ message: 'Test' });
 		});
